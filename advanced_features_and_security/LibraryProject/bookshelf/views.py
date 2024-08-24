735a2aead8e8ba249c784b108acaaed37cf900b0
betelhem demsis <bettydemsis@gmail.com>
2024-08-24 21:39:43 +0300

    modified

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import YourModel
+
+@permission_required('yourapp.can_edit', raise_exception=True)
+def edit_view(request, pk):
+    instance = get_object_or_404(YourModel, pk=pk)
+    return render(request, 'edit_template.html', {'instance': instance})
+
+@permission_required('yourapp.can_create', raise_exception=True)
+def create_view(request):
+    return render(request, 'create_template.html')
+
+@permission_required('yourapp.can_delete', raise_exception=True)
+def delete_view(request, pk):
+    instance = get_object_or_404(YourModel, pk=pk)
+    instance.delete()
+   
