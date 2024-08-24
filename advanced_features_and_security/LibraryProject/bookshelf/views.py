50b0820fd3dac0215fbe5c79ed552400e8ff8644
betelhem demsis <bettydemsis@gmail.com>
2024-08-24 21:45:35 +0300

    modified:   advanced_features_and_security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render
 
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
-from .models import YourModel
+from .models import Book
 
-@permission_required('yourapp.can_edit', raise_exception=True)
+
+def book_list(request):
+    books = Book.objects.all() 
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_view(request, pk):
-    instance = get_object_or_404(YourModel, pk=pk)
+    instance = get_object_or_404(Book, pk=pk)
     return render(request, 'edit_template.html', {'instance': instance})
 
-@permission_required('yourapp.can_create', raise_exception=True)
+@permission_required('bookshelf.can_create', raise_exception=True)
 def create_view(request):
     return render(request, 'create_template.html')
 
-@permission_required('yourapp.can_delete', raise_exception=True)
+@permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_view(request, pk):
-    instance = get_object_or_404(YourModel, pk=pk)
+    instance = get_object_or_404(Book, pk=pk)
     instance.delete()
    
