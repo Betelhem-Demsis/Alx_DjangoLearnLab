21b80a32dbc85cf6eb600daee999dc99bacd418a
betelhem demsis <bettydemsis@gmail.com>
2024-09-04 16:11:23 +0300

    advanced_api_project

---
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from .serializers import BookSerializer
+from rest_framework import permissions
+from .permissions import IsAuthorOrReadOnly
+
+# Create your views here.
+
+class BookListView(generics.ListAPIView):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['publication_year', 'author__name']
+    ordering_fields = ['title', 'publication_year']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        title = self.request.query_params.get('title', None)
+        if title is not None:
+            queryset = queryset.filter(title__icontains=title)
+        return queryset
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+class BookCreateView(generics.CreateAPIView):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
+    def perform_create(self, serializer):
+        serializer.save()
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
+     permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
