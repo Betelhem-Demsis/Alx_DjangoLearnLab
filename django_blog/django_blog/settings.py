4b60a8b7fad757fe44c9d104d1a47c8400c61c61
betelhem demsis <bettydemsis@gmail.com>
2024-09-11 00:14:04 +0300

    modified

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,6 +58,8 @@ STATIC_URL= '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -82,6 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        "USER":os.getenv("DB_USER"),
+        "PASSWORD":os.getenv("DB_PASSWORD"),
+        "HOST":os.getenv("DB_HOST"),
+        "PORT":os.getenv("DB_PORT"),
     }
 }
 
